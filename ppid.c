2a5889ec4bbf4e940175fb616546b8b0d4c25fc1
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-18 14:14:45 +0100

    ppid

---
+++ ppid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - getppid
+* Retuen: 0 successful
+*/
+int main(void)
+{
+pid_t parent_pid;
+
+parent_pid = getppid();
+printf("Parent PID: %d\n", parent_pid);
+return (0);
+}

7208456946ea896f45966c4c969dae9627dcdc43
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-23 10:11:04 +0100

    hsh path

--- search_path.c
+++ search_path.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 /**
 */
+
 char *search_path(char *command) {
     char *path = getenv("PATH");
     char *token;
@@ -10,8 +11,14 @@ char *search_path(char *command) {
     }
     token = strtok(path, ":");
     while (token != NULL) {
-        char *full_path = malloc(MAX_COMMAND_LENGTH);
-        snprintf(full_path, MAX_COMMAND_LENGTH, "%s/%s", token, command);
+        char *full_path = malloc(strlen(token) + strlen(command) + 2);
+        if (!full_path) {
+            fprintf(stderr, "allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(full_path, token);
+        strcat(full_path, "/");
+        strcat(full_path, command);
         if (access(full_path, X_OK) == 0) {
             return (full_path);
         }

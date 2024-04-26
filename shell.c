97d9f21be9c09fbbd864f1b0a0557a0e63d981e9
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-26 09:49:45 +0100

    main fuction modification

--- shell.c
+++ shell.c
@@ -8,27 +8,34 @@ int main(void)
 {
 char *line = NULL;
 char **args;
+char *commands;
 
 while (1)
-{
-if (isatty(STDOUT_FILENO))
-{
-printf("$ ");
-fflush(stdout);
-}
-read_input(&line);
-if (strlen(line) == 0)
-{
-free(line);
-continue;
-}
+    {
+        if (isatty(STDOUT_FILENO))
+        {
+            printf("$ ");
+            fflush(stdout);
+        }
+        read_input(&line);
+        if (strlen(line) == 0)
+        {
+            free(line);
+            continue;
+        }
 
-args = split_line(line);
-execute_command(args);
-free(line);
-free(args);
-if (!isatty(STDOUT_FILENO))
-break;
-}
-return (EXIT_SUCCESS);
+        commands = strtok(line, "\n");
+        while (commands != NULL)
+        {
+            args = split_line(commands);
+            execute_command(args);
+            free(args);
+            commands = strtok(NULL, "\n");
+        }
+
+        free(line);
+        if (!isatty(STDOUT_FILENO))
+            break;
+    }
+    return (EXIT_SUCCESS);
 }
